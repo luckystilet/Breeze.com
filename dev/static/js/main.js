bbc03aeb51c06db891c2f660791094aabd78e4bb
Alex <luckystilet@gmail.com>
2019-06-27 23:17:09 +0300

    На много улучшил адаптацию, увеличивающийся поиск на мобильном, идеальная работа все попапов при любых изменениях экрана, все лисенеры добавляются только когда нужно, а потом удаляются. Добавлены карточки товара

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,5 +1,147 @@
-$(document).ready(function(){
+const debounce = (callback, delay) => {
+	let timerId;
+	return (...args) => {
+		timerId && clearTimeout(timerId);
+		timerId = setTimeout(
+			() => callback(...args),
+			delay
+			);
+	}
+};
+let languagesItems = $('.languages__item'),
+languagesMobile = $('.languages__mobile'),
+currencyMobile = $('.currency__mobile'),
+currencyItems = $('.currency__item'),
+doc = $(document),
+documentClickLang,
+langItemsClick,
+closeLoginClick,
+closeSignupClick;
 
+
+
+let activeCloseLogin = debounce(function(){
+	$('.login').toggleClass('form--active');
+	let documentClickCloseLogin;
+	closeLoginClick = $('#close-login').click(function(){
+		console.log('Закрытие логина по клику по кнопке');
+		$('.login').removeClass('form--active');
+		$('#close-login').off('click');
+		documentClickCloseLogin.off('click');
+	});
+
+
+	documentClickCloseLogin = doc.click(function(event){
+		console.log('Закрытие логина по клику вне блока');
+		if ($(event.target).closest($('.login')).length) return;
+		$('.login').removeClass('form--active');
+		// event.stopPropagation();
+		closeLoginClick.off('click');
+		documentClickCloseLogin.off('click');
+	});
+},200);
+
+
+let activeCloseSignup = debounce(function(){
+	$('.signup').toggleClass('form--active');
+	let documentClickCloseSignup;
+	closeSignupClick = $('#close-signup').click(function(){
+		$('.signup').removeClass('form--active');
+		closeSignupClick.off('click');
+		documentClickCloseSignup.off('click');
+	});
+
+	documentClickCloseSignup = doc.click(function(event) {
+		console.log('click documentClickCloseSignup');
+		if ($(event.target).closest($('.signup')).length) return;
+		$('.signup').removeClass('form--active');
+		documentClickCloseSignup.off('click');
+		closeSignupClick.off('click')
+	});
+},200);
+
+function activeLangSelection(){
+	langItemsClick = languagesItems.click(function(){
+		console.log('Клик по айтему языка');
+		languagesItems.removeClass('languages__item--active');
+		$(this).addClass('languages__item--active');
+		updateLangMob();
+		$('.languages__list').removeClass('languages__list--active');
+		languagesItems.off('click');
+		documentClickLang.off('click');
+	});
+
+	documentClickLang = doc.click(function(event) {
+		console.log('Клик вне блока языков');
+		if ($(event.target).closest(languagesItems).length) return;
+		$('.languages__list').removeClass('languages__list--active');
+		languagesItems.off('click');
+		documentClickLang.off('click');
+	});
+}//activeLangSelection
+
+
+function activeCurSelection(){
+	let documentClickCur;
+	currencyItems.click(function(){
+		console.log('Клик по айтему выбора валюты');
+		currencyItems.removeClass('currency__item--active');
+		$(this).addClass('currency__item--active');
+		updateCurMob();
+		updatePriceCur();
+		$('.currency__list').removeClass('currency__list--active');
+		currencyItems.off('click');
+		documentClickCur.off('click');
+	});
+
+	documentClickCur = doc.click(function(event) {
+		console.log('Клик вне блока выбора валюты');
+		if ($(event.target).closest(currencyItems).length) return;
+		$('.currency__list').removeClass('currency__list--active');
+		documentClickCur.off('click');
+		currencyItems.off('click');
+	});
+}//activeCurSelection
+
+function activeCurSelectionDT(){
+	currencyItemsClick = currencyItems.click(function(){
+		console.log('Клик по айтему валюты DT');
+		currencyItems.removeClass('currency__item--active');
+		$(this).addClass('currency__item--active');
+		updateCurMob();
+		updatePriceCur();
+		$('.currency__list').removeClass('currency__list--active');
+	});
+}//activeCurSelectionDT
+
+function activeLangSelectionDT(){
+	langItemsClick = languagesItems.click(function(){
+		console.log('Клик по айтему языка DT');
+		languagesItems.removeClass('languages__item--active');
+		$(this).addClass('languages__item--active');
+		updateLangMob();
+		$('.languages__list').removeClass('languages__list--active');
+	});
+}//activeLangSelectionDT
+
+function updateLangMob(){
+	let langAct = $('.languages__item--active').text();
+	$('.languages__mobile').text(langAct);
+}
+
+function updateCurMob(){
+	let curAct = $('.currency__item--active').text();
+	$('.currency__mobile').text(curAct);
+}
+
+function updatePriceCur(){
+	$('.cart__currency').text($('.currency__item--active').text());
+}
+
+
+
+$(document).ready(function(){
+	// ======  SLICK   ======
 	$('.slider-js').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
@@ -17,156 +159,85 @@ $(document).ready(function(){
 		}
 	});
 
-	let languagesItems = $('.languages__item'),
-	languagesMobile = $('.languages__mobile'),
-	currencyMobile = $('.currency__mobile'),
-	currencyItems = $('.currency__item');
-
-
-	function activeCloseLogin(){
-		$('#close-login').click(function(){
-			$('.login').removeClass('form--active');
-			$('#close-login').off('click');
-			$(document).off('click');
-		});
-
-		window.setTimeout(function(){
-			$(document).click(function(event) {
-				if ($(event.target).closest($('.login')).length) return;
-				$('.login').removeClass('form--active');
-				event.stopPropagation();
-				$(document).off('click');
-			});
-		},100);
-	}
-
-
-	function activeCloseSignup(){
-		$('#close-signup').click(function(){
-			$('.signup').removeClass('form--active');
-			$('#close-signup').off('click');
-			$(document).off('click');
-		});
-
-		window.setTimeout(function(){
-			$(document).click(function(event) {
-				if ($(event.target).closest($('.signup')).length) return;
-				$('.signup').removeClass('form--active');
-				event.stopPropagation();
-				$(document).off('click');
-			});
-		},100);
-	}
-
-	// set content of mobile btns
-	function updateLangMob(){
-		let langAct = $('.languages__item--active').text();
-		$('.languages__mobile').text(langAct);
-	}
-
-	function updateCurMob(){
-		let curAct = $('.currency__item--active').text();
-		$('.currency__mobile').text(curAct);
-	}
-
-	function updatePriceCur(){
-		$('.cart__currency').text($('.currency__item--active').text());
-	}
-
-	function activeLangSelection(){
-		languagesItems.click(function(){
-			languagesItems.removeClass('languages__item--active');
-			$(this).addClass('languages__item--active');
-			updateLangMob();
-			window.setTimeout(function(){
-				$('.languages__list').removeClass('languages__list--active');
-			}, 300);
-			$(document).off('click');
-		});
-
-		$(document).click(function(event) {
-			if ($(event.target).closest(languagesItems).length) return;
-			$('.languages__list').removeClass('languages__list--active');
-			event.stopPropagation();
-			$(document).off('click');
-		});
-	}
-
-
-	function activeCurSelection(){
-		currencyItems.click(function(){
-			currencyItems.removeClass('currency__item--active');
-			$(this).addClass('currency__item--active');
-			updateCurMob();
-			updatePriceCur();
-			window.setTimeout(function(){
-				$('.currency__list').removeClass('currency__list--active');
-			}, 300);
-			$(document).off('click');
-		});
-
-		$(document).click(function(event) {
-			if ($(event.target).closest(languagesItems).length) return;
-			$('.currency__list').removeClass('currency__list--active');
-			event.stopPropagation();
-			$(document).off('click');
-		});
-	}
+		$('.best-sellers-js').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		// prevArrow: '.slider__controls--prev',
+		// nextArrow: '.slider__controls--next',
+		easing: 'swing',
+		speed: 700,
+		// appendDots: $('.slider__dots'),
+		dots: true,
+		// dotsClass: 'custom-dots',
+		// customPaging: function (slider, i) {
+		// 	var slideNumber = (i + 1),
+		// 	totalSlides = slider.slideCount;
+		// 	return '<a class="slider__dot" role="button" title="' + slideNumber + ' of ' + totalSlides + '"></a>';
+		// }
+	});
 
 
 
+
+
+	// ======  SLICK   ======
 
 	$('.user-menu__link--signup').click(function(){
-		$('.signup').toggleClass('form--active');
 		activeCloseSignup();
 	});
 	$('.greeting__link--signup').click(function(){
-		$('.signup').addClass('form--active');
 		activeCloseSignup();
 	});
-	languagesMobile.click(function(){
-		$('.languages__list').addClass('languages__list--active');
-		window.setTimeout(activeLangSelection, 50);
-	});
-	currencyMobile.click(function(){
-		$('.currency__list').addClass('currency__list--active');
-		window.setTimeout(activeCurSelection, 50);
-	});
 	$('.user-menu__link--login').click(function(){
-		$('.login').toggleClass('form--active');
 		activeCloseLogin();
 	});
 	$('.greeting__link--login').click(function(){
-		$('.login').toggleClass('form--active');
 		activeCloseLogin();
 	});
 
-	activeLangSelection();
-	activeCurSelection();
+
+	let languagesMobileClick = debounce(function(){
+		console.log('клик по кнопке открытия мобильного языка');
+		$('.languages__list').addClass('languages__list--active');
+		activeLangSelection();
+	},200);
+	languagesMobile.click(function(){
+		languagesMobileClick();
+	});
+
+
+	let currencyMobileClick = debounce(function(){
+		console.log('клик по валюта мобила');
+		$('.currency__list').addClass('currency__list--active');
+		activeCurSelection();
+	},200);
+	currencyMobile.click(function(){
+		currencyMobileClick();
+	});
+
+	activeLangSelectionDT();
+	activeCurSelectionDT();
 	updateLangMob();
 	updateCurMob();
-});
+}); //$(document).ready
 
-
-
-const debounce = (callback, delay) => {
-	let timerId;
-	return (...args) => {
-		timerId && clearTimeout(timerId);
-		timerId = setTimeout(
-			() => callback(...args),
-			delay
-			);
-	}
-};
+// Переменная smallSizeWasActivated указывающая что уже были активированы ивенты для маленького размера,
+// Если это так - только тогда удаляются ивенты после ресайза
+let smallSizeWasActivated = false,
+mql = window.matchMedia('all and (max-width: 780px)'),
+small = window.matchMedia('all and (max-width: 480px)'),
+mobileLangCurSize = window.matchMedia('all and (min-width: 641px)'),
+search = $('.search'),
+searchInput = $('.search .input-field'),
+cart = $('.cart'),
+searchIcon = $('.search__icon'),
+searchIconClick,
+searchDocClick;
 
 let resizeActions = debounce(function() {
-	let mql = window.matchMedia('all and (max-width: 780px)'),
-	cart = $('.cart');
-	let small = window.matchMedia('all and (max-width: 480px)');
-	let search = $('.search');
-
-	if(mql.matches){
+	console.log('Запущен resizeActions');
+	searchInput.off('click');
+	if(mql.matches){ //max-width: 780px
 		$('.search').after(cart);
 		$('.header-middle__inner').after($('.user-menu'));
 	}else{
@@ -174,44 +245,66 @@ let resizeActions = debounce(function() {
 		$('.header-middle__inner').append($('.user-menu'));
 	}
 
-	if(small.matches){
-		let searchIconClick,
-		searchDocClick;
-		$('.search .input-field').attr('placeholder', 'Search...');
-		search.click(function(event){
-			event.stopPropagation();
-			search.addClass('search--active');
-			$('.search .input-field').select();
-			window.setTimeout(function(){
-				searchDocClick = $(document).click(function(event) {
-					if ($(event.target).closest($('.search')).length){
-						console.log('document click IF');
-						return
-					};
-					console.log('document click AFTER IF');
-					$('.search').removeClass('search--active');
-					event.stopPropagation();
-					searchDocClick.off('click');
-					searchIconClick.off('click');
-				});
-			},100);
+	if(mobileLangCurSize.matches){ //min-width: 641px
+		langItemsClick.off('click');
+		currencyItemsClick.off('click');
+		activeLangSelectionDT();
+		activeCurSelectionDT();
+	}
 
-			// window.setTimeout(function(){
-				searchIconClick = $('.search__icon').click(function(){
+	if(small.matches){
+		smallSizeWasActivated = true;
+
+		searchInput.attr('placeholder', 'Search...');
+		searchActions();
+
+		function searchActions(){
+			console.log('searchActions');
+			//input Search
+			searchInput.click(function(event){
+				searchInput.off('click');
+				console.log('Клик по searchInput');
+				search.addClass('search--active');
+				window.setTimeout(function(){
+					// document click to close
+					searchDocClick = $(document).click(function(event) {
+						if ($(event.target).closest($('.search')).length){
+							console.log('document click отфильтрован как клик по поиску.');
+							return
+						};
+						console.log('Клик вне Search input');
+						search.removeClass('search--active');
+						event.stopPropagation();
+						searchDocClick.off('click');
+						searchIconClick.off('click');
+						searchInput.off('click');
+						searchActions();
+					});
+				},100);
+				// Search Icon
+				searchIconClick = searchIcon.click(function(){
 					console.log('search__icon click');
 					$('.search').removeClass('search--active');
 					searchIconClick.off('click');
 					searchDocClick.off('click');
+					searchInput.off('click');
+					searchActions();
 				});
-			// },100);
-
-		});
+			});
+		} //max-width: 480px
 	}else{
-		$('.search .input-field').attr('placeholder', 'Search entire store here...');
-		search.removeClass('search--active')
-		search.off('click');
-		$(document).off('click');
+		if(smallSizeWasActivated){
+			console.log("экран больше 480");
+			smallSizeWasActivated = false;
+			$('.search .input-field').attr('placeholder', 'Search entire store here...');
+			search.removeClass('search--active');
+			searchInput.off('click');
+			searchDocClick.off('click');
+			searchIconClick.off('click');
+		}
 	}
+
+
 	// Расчет высоты слайда, пропорционально ширине по коэффициенту
 	let slideW = $('.slider__item-inner').outerWidth();
 	$('.slider__item-inner').outerHeight(slideW*0.4878);
@@ -221,7 +314,7 @@ let resizeActions = debounce(function() {
 	// Расчет высоты элемента блока преймущества, пропорционально ширине по коэффициенту
 	let productAdvW = $('.product-advantages__item').outerWidth();
 	$('.product-advantages__item').outerHeight(productAdvW*0.3174);
-}, 150);
+}, 300);
 
 
 $(window).on('resize load change orientationchange', function(){
